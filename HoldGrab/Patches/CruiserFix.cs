60ef5cc4fa47ecf9d26ae6ad52530205f14e8aa4
TI <115467358+tixomirof@users.noreply.github.com>
2025-02-16 00:11:22 +0500

    fix: cruiser instant leave on seat

---
+++ HoldGrab/Patches/CruiserFix.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+
+namespace HoldGrab.Patches
+{
+    [HarmonyPatch(typeof(VehicleController))]
+    public class CruiserFix
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(VehicleController.SetPlayerInControlOfVehicleClientRpc))]
+        static void PatchTakeControl(VehicleController __instance)
+        {
+            if (GameNetworkManager.Instance.localPlayerController != __instance.currentDriver
+                && GameNetworkManager.Instance.localPlayerController != __instance.currentPassenger)
+                return;
+
+            General.GrabPostInteractTimer = 0f;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(VehicleController.SetPassengerInCar))]
+        static void PatchSetPassenger(VehicleController __instance)
+        {
+            if (GameNetworkManager.Instance.localPlayerController != __instance.currentPassenger)
+                return;
+
+            General.GrabPostInteractTimer = 0f;
+        }
+    }
+}
